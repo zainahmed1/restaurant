066aff16ffc2d1eb933750160651c971bdc8bcc0
zainahmed1 <50447349+zainahmed1@users.noreply.github.com>
2019-07-16 04:05:44 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,402 @@
+let restaurant = {
+    name: "Kebabistan",
+    since: "20/September/2018",
+    location: "Plot # SB 8, Near Disco Bakery, Allama Shabbir Ahmed Usmani Rd, Block 4 Gulshan-e-Iqbal, Karachi",
+    timing: {
+        monToFri: "2pm To 12pm",
+        satToSun: "4pm To 2am",
+
+    },
+    ownerDetail: {
+        name: "",
+        fatherName: "",
+        dateOfBirth: "",
+        contact: "0324-5678920",
+        email: "@gmail.com",
+
+    },
+
+    restaurantEmail: "Kebabistan123@gmail.com",
+    webSite: "www.Kebabistan.com",
+    restaurantContact: "(051) 111 3333 444",
+    quantityOfVaitors: 200,
+    quantityOfChief: 100,
+    quantityOfTotalStaff: 500,
+    quantityOfDishes: 100,
+    ceoAndPartner: {
+        ceo:
+        {
+            name: "Talha Bin Shuja",
+            work: "The Entrepreneur",
+        },
+        partner: {
+            name: "Mohammed Ibrahim",
+            work: "The Market Expert",
+        }
+    },
+    mentor: {
+        mentor: "Arshad Bin Ali",
+        mentor: "Shujauddin",
+    },
+    operationManagersOrDesigner: {
+        operationManager: {
+            name: "Waqas Nagari",
+            work: "The Food Expert",
+        },
+        creativeExternalDesigner: {
+            name: "Shabir Ahmed Chugtai",
+            work: "The Architect",
+        }
+
+    },
+    menuList: {
+        bbqDishes: {
+            heeraGrill: {
+                halfPlate: "600 Rs",
+                fullPlate: "1200 Rs",
+            },
+            kastoriBoti: {
+                halfPlate: "410 Rs",
+                fullPlate: "820 Rs",
+            },
+            chickenMalaiBoti: {
+                halfPlate: "360 Rs",
+                fullPlate: "740 Rs",
+            },
+            chickenLebaneseboti: {
+                halfPlate: "350 Rs",
+                fullPlate: "700 Rs",
+            },
+            chickenAchariBoti: {
+                halfPlate: "400 Rs",
+                fullPlate: "600 Rs",
+            },
+            chickenTandooriBoti: {
+                halfPlate: "390 Rs",
+                fullPlate: "700 Rs",
+            },
+            afghaniBoti: {
+                halfPlate: "360 Rs",
+                fullPlate: "720 Rs",
+            },
+            kebabistanSpecialMuttonHanju: {
+                halfPlate: "400 Rs",
+                fullPlate: "800 Rs",
+            },
+            specialHunzaiKebab: {
+                halfPlate: "500 Rs",
+                fullPlate: "1000 Rs",
+            },
+            beefSeekhKebab: {
+                halfPlate: "280 Rs",
+                fullPlate: "560 Rs",
+
+            },
+            chickenReshmiKebab: {
+                halfPlate: "360 Rs",
+                fullPlate: "720 Rs",
+            },
+            chickenSpicyReshmiKebab: {
+                halfPlate: "400 Rs",
+                fullPlate: "800 Rs",
+            },
+            chickenSeekhKebab: {
+                halfPlate: "310 Rs",
+                fullPlate: "620 Rs",
+            },
+            chickenNargasiKebab: {
+                halfPlate: "399 Rs",
+                fullPlate: "799 Rs",
+            },
+            beefBihariKebab: {
+                halfPlate: "349 Rs",
+                fullPlate: "699 Rs",
+            },
+            lahmKebab: {
+                halfPlate: "360 Rs",
+                fullPlate: "720 Rs",
+            },
+            chandonKebab: {
+                halfPlate: "340 Rs",
+                fullPlate: "680 Rs",
+            },
+            chelowKebabBeef: {
+                halfPlate: "390 Rs",
+                fullPlate: "780 Rs",
+            },
+            chelowKebabMutton: {
+                halfPlate: "580 Rs",
+                fullPlate: "1060 Rs",
+            },
+            chelowKebabChicken: {
+                halfPlate: "490 Rs",
+                fullPlate: "980 Rs",
+            },
+            kebabistanSpecialTikka: {
+                halfPlate: "250 Rs",
+                fullPlate: "500 Rs",
+            },
+            kabuliTikka: {
+                halfPlate: "220 Rs",
+                fullPlate: "440 Rs",
+            },
+            chickenTikka: {
+                halfPlate: "199 RS",
+                fullPlate: "399 Rs",
+            },
+            fishTikka: {
+                halfPlate: "430 Rs",
+                fullPlate: "660 Rs",
+            }
+        },
+        chineseDishes: {
+            kebabistanSpecialSoupSingle: {
+                halfPlate: "209 Rs",
+                fullPlate: "419 Rs",
+            },
+            chickenCornSoupSingle: {
+                halfPlate: "169 Rs",
+                fullPlate: "340 Rs",
+            },
+            hotAndSourSoupSingle: {
+                halfPlate: "179 Rs",
+                fullPlate: "360 Rs",
+            },
+            kebabistanSpecialSoupFamily: {
+                halfPlate: "609 Rs",
+                fullPlate: "1220 Rs",
+            },
+            chickenCornSoupFamily: {
+                halfPlate: "509 Rs",
+                fullPlate: "1020 Rs",
+            },
+            hotAndSourSoupFamily: {
+                halfPlate: "559 Rs",
+                fullPlate: "1020 Rs",
+            },
+            kebabistanSpecialChowmein: {
+                halfPlate: "409 Rs",
+                fullPlate: "820 Rs",
+            },
+            chickenChowmein: {
+                halfPlate: "359 Rs",
+                fullPlate: "720 Rs",
+            },
+            vegetableChowmein: {
+                halfPlate: "269 Rs",
+                fullPlate: "540 Rs",
+            },
+            fingerFish: {
+                halfPlate: "349 Rs",
+                fullPlate: "700 Rs",
+            },
+            dynamitePrawns: {
+                halfPlate: "379 Rs",
+                fullPlate: "760 Rs",
+            },
+            manchurianWithRice: {
+                halfPlate: "430 Rs",
+                fullPlate: "860 Rs",
+            },
+            shashlikWithRice: {
+                halfPlate: "430 Rs",
+                fullPlate: "860 Rs",
+            },
+            fishManchiranWithRice: {
+                halfPlate: "449 Rs",
+                fullPlate: "900 Rs",
+            },
+            fishShashlikWithRice: {
+                halfPlate: "429 Rs",
+                fullPlate: "860 Rs",
+            },
+            appleFish: {
+                halfPlate: "420 Rs",
+                fullPlate: "840 Rs",
+            },
+            kungPaoChicken: {
+                halfPlate: "409 Rs",
+                fullPlate: "820 Rs",
+            },
+            schezwanChicken: {
+                halfPlate: "409 Rs",
+                fullPlate: "820 Rs",
+            },
+            kebabistanSpecialRice: {
+                halfPlate: "339 Rs",
+                fullPlate: "680 Rs",
+            },
+            chickenFriedRice: {
+                halfPlate: "289 Rs",
+                fullPlate: "580 Rs",
+            },
+            vegetableFriedRice: {
+                halfPlate: "249 Rs",
+                fullPlate: "500 Rs",
+            },
+            eggFriedRice: {
+                halfPlate: "239 Rs",
+                fullPlate: "480 Rs",
+            },
+            plainRice: {
+                halfPlate: "130 Rs",
+                fullPlate: "260 Rs",
+            }
+        },
+        desiBar: {
+            KebabistanSpecialBadamIlaichiSharbat: {
+                singleGlass: "70 Rs",
+                doubleGlass: "100 Rs",
+            },
+            Shikanjabeen: {
+                singleGlass: "90 Rs",
+                doubleGlass: "150 Rs",
+            },
+            NamkeenLassi: {
+                singleGlass: "60 Rs",
+                doubleGlass: "100 Rs",
+            },
+            MethiLassi: {
+                singleGlass: "80 Rs",
+                doubleGlass: "140 Rs",
+            },
+            Mojito: {
+                singleGlass: "130 Rs",
+                doubleGlass: "240 Rs",
+            },
+            BlueLagoon: {
+                singleGlass: "160 Rs",
+                doubleGlass: "280 Rs",
+            },
+            MintLemonade: {
+                singleGlass: "209 Rs",
+                doubleGlass: "300 Rs",
+            },
+            BlueLemonade: {
+                singleGlass: "209 Rs",
+                doubleGlass: "380 Rs",
+            },
+            StrawberryLemonade: {
+                singleGlass: "209 Rs",
+                doubleGlass: "350 Rs",
+            },
+            PeachLemonade: {
+                singleGlass: "209 Rs",
+                doubleGlass: "350 Rs",
+            },
+            KebabistanSpecialIceTea: {
+                singleGlass: "130 Rs",
+                doubleGlass: "200 Rs",
+            },
+            MatkaKulfi: {
+                singleGlass: "50 Rs",
+                doubleGlass: "80 Rs",
+            }
+        },
+        karahi: {
+            chickenOliveKarahi: {
+                halfPlate: "510 Rs",
+                fullPlate: "960 Rs",
+            },
+            chickenDesiKarahi: {
+                halfPlate: "490 Rs",
+                fullPlate: "950 Rs",
+            },
+            chickenPeshawariKarahi: {
+                halfPlate: "460 Rs",
+                fullPlate: "860 Rs",
+            },
+            chickenWhiteKarahi: {
+                halfPlate: "510 Rs",
+                fullPlate: "910 Rs",
+            },
+            muttonDesiKarahi: {
+                halfPlate: "740 Rs",
+                fullPlate: "1360 Rs",
+            },
+            muttonPeshawariKarahi: {
+                halfPlate: "710 Rs",
+                fullPlate: "1310 Rs",
+            },
+            muttonWhiteKarahi: {
+                halfPlate: "760 Rs",
+                fullPlate: "1410 Rs",
+            },
+            handi: {
+                reshmiHandi: {
+                    halfHandi: "450 Rs",
+                    fullHandi: "900 Rs",
+                },
+                chickenShahjahaniHandi: {
+                    halfHandi: "450 Rs",
+                    fullHandi: "900 Rs ",
+                },
+                murghMakhaniHandi: {
+                    halfHandi: "400 Rs",
+                    fullHandi: "800 Rs",
+                },
+                bbqGrillHandi: {
+                    halfHandi: "350 Rs",
+                    fullHandi: "700 Rs",
+                },
+                chickenAchariHandi: {
+                    halfHandi: "800 Rs",
+                    fullHandi: "1600 Rs",
+                },
+                chickenMughlaiHandi: {
+                    halfHandi: "750 Rs",
+                    fullHandi: "1500 Rs",
+                },
+            },
+            rice: {
+                afghaniPulao: {
+                    singlePlate: "299 Rs",
+                    doublePlate: "600 Rs",
+                },
+                uzbakiPulao: {
+                    singlePlate: "299 Rs",
+                    doublePlate: "600 Rs",
+                },
+                deserts: {
+                    kheer: {
+                        badamiKheer: "80 Rs",
+                        pistaKheer: "120 Rs",
+                    },
+                    beverages: {
+                        can: "69 Rs",
+                        aquaFinaSmall: "39 Rs",
+                        aquaFinaLarge: "59 Rs",
+                        freshLime7upWithLemon: "69 Rs",
+                    },
+                    sideLines: {
+                        greenSalad: "60 Rs",
+                        Raita: "60 Rs",
+                    },
+                    tandoor:{
+                        kebabistanSpecialDilWalaNan:"40 Rs",
+                        garlicNan:"30 Rs",
+                        plainNan:"20 Rs",
+                        puriParatha: "30 Rs",
+                    },
+                    specialities:{
+                        hanjuKebabMuttonkebabs:"1000 Rs",
+                        kastooriBoti:"1200 Rs",
+                        chandonkebab:"1300 Rs",
+                        fishTikka:"800 Rs",
+                        achariBoti:"700 Rs",
+                        specialGrill:"900 Rs",
+                        specialPlatter:"800 Rs",
+                        chulluKebab:"400 Rs",
+                        reshmiHandi:"1000 Rs",
+                        desiAngethi:"1600 Rs",
+                        kebabistanSpecialPlatter:"1500 Rs",
+                        kebabistanSpecialGrill:"2000 Rs",
+                    },
+
+                }
+            }
+        },
+
+    },
+
+}
